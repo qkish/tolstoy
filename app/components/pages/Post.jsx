b43a820566f792352eb880dba9468d005c978dbb
qkish <gqkish@gmail.com>
2017-02-01 12:39:16 +0300

    Commit

--- app/components/pages/Post.jsx
+++ app/components/pages/Post.jsx
@@ -153,14 +153,7 @@ class Post extends React.Component {
                 </div>
                 {!current_user && <div className="row">
                     <div className="column">
-                        <div className="Post__promo">
-                            {translate('authors_get_paid_when_people_like_you_upvote_their_post')}.
-                            <br /> {// remove '$' from signup_bonus before parsing it into local currency
-                                    translate('if_you_enjoyed_what_you_read_earn_amount', {amount: localizedCurrency(signup_bonus.substring(1))})}
-                            <br /> {translate('when_you') + ' '}
-                            <a onClick={showSignUp}>{translate('when_you_link_text')}</a>
-                            {' ' + translate('and_vote_for_it') + '.'}
-                        </div>
+                        
                     </div>
                 </div>}
                 <div id="comments" className="Post_comments row hfeed">
