e9f39d3c155ebe39cb845e96dce71c8e09e8ce74
Eli Streit <elistreit@outlook.com>
2017-02-19 14:07:25 +0300

    DB Migrations for Raitings

---
+++ db/models/groups.js
@@ -0,0 +1,35 @@
+export default (sequelize, DataTypes) => {
+	
+	const Group = sequelize.define('Group', {
+		UserId: {
+			type: DataTypes.INTEGER,
+			references: {
+				model: 'users',
+				key: 'id'
+			},
+			field: 'user_id',
+		},
+		type: DataTypes.ENUM(['polk', 'hundred', 'ten', 'couch']),
+		money: DataTypes.INTEGER,
+		vesting: DataTypes.INTEGER
+	}, {
+		// TODO: Move createdAt, updatedAt, timestamps, underscored to global Sequelize define
+		tableName: 'groups',
+		createdAt: 'created_at',
+		updatedAt: 'updated_at',
+		timestamps: true,
+		underscored: true,
+		classMethods: {
+			associate: (models) => {
+				Group.belongsTo(models.User, {
+					onDelete: "CASCADE",
+					foreignKey: {
+						allowNull: false
+					},
+				})
+			}
+		}
+	})
+	
+	return Group
+}
